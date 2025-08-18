5c1c94aa0411fab5f15c12917bdd0f47d7fa1753
rafael cemin <rafaelcemin06@gmail.com>
2025-08-18 20:54:36 -0300

    Fix indentation

--- prog1/tp1/racional.c
+++ prog1/tp1/racional.c
@@ -1,11 +1,11 @@
 /* 
 
- * Arquivo de implementação para TAD racional.
- * Feito em 16/09/2024 para a disciplina CI1001 - Programação 1.
- *
- * Este arquivo deve conter as implementações das funções cujos protótipos
- * foram definidos em racional.h. Neste arquivo também podem ser definidas
- * funções auxiliares para facilitar a implementação daquelas funções.
+* Arquivo de implementação para TAD racional.
+* Feito em 16/09/2024 para a disciplina CI1001 - Programação 1.
+*
+* Este arquivo deve conter as implementações das funções cujos protótipos
+* foram definidos em racional.h. Neste arquivo também podem ser definidas
+* funções auxiliares para facilitar a implementação daquelas funções.
 */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
@@ -16,58 +16,54 @@
 
 
 /*
- * Implemente aqui as funcoes definidas no racionais.h; caso precise,
- * pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
- * somente neste arquivo.
+* Implemente aqui as funcoes definidas no racionais.h; caso precise,
+* pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
+* somente neste arquivo.
 */
 
 /* retorna um número aleatório entre min e max, inclusive. */
-long aleat (long min, long max)
-{
+long aleat (long min, long max) {
 	return rand() % (max - min + 1) + min; 
 }
 
 /* Máximo Divisor Comum entre a e b      */
 /* calcula o MDC pelo método de Euclides */
-long mdc (long a, long b)
-{
-  int temp;
-  while (b != 0) {
-    a %= b;
-    temp = a;
-    a = b;
-    b = temp;
-  }
-  if (a > 0)
-  	return a;
-  return (-1 * a);
+long mdc (long a, long b) {
+	int temp;
+	while (b != 0) {
+		a %= b;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a > 0)
+	return a;
+	return (-1 * a);
 }
 
 /* Mínimo Múltiplo Comum entre a e b */
 /* mmc = (a * b) / mdc (a, b)        */
-long mmc (long a, long b)
-{
-  return (a * b) / mdc (a, b);
+long mmc (long a, long b) {
+	return (a * b) / mdc (a, b);
 }
 
 /* Recebe um número racional e o simplifica.
- * Por exemplo, ao receber 10/8 deve retornar 5/4.
- * Se ambos numerador e denominador forem negativos, deve retornar um positivo.
- * Se o denominador for negativo, o sinal deve migrar para o numerador.
- * Se r for inválido, devolve-o sem simplificar. */
-struct racional simplifica_r (struct racional r)
-{
-  int max_div;
-  if (valido_r(r) == 0)
-    	return r;
-  max_div = mdc(r.num, r.den);
-  r.num /= max_div;
-  r.den /= max_div;
-  if (r.den < 0) {
-    r.num *= -1;
-    r.den *= -1;
-  }
-  return r;
+* Por exemplo, ao receber 10/8 deve retornar 5/4.
+* Se ambos numerador e denominador forem negativos, deve retornar um positivo.
+* Se o denominador for negativo, o sinal deve migrar para o numerador.
+* Se r for inválido, devolve-o sem simplificar. */
+struct racional simplifica_r (struct racional r) {
+	int max_div;
+	if (valido_r(r) == 0)
+	return r;
+	max_div = mdc(r.num, r.den);
+	r.num /= max_div;
+	r.den /= max_div;
+	if (r.den < 0) {
+		r.num *= -1;
+		r.den *= -1;
+	}
+	return r;
 }
 
 /* implemente as demais funções aqui */
@@ -81,7 +77,7 @@ struct racional cria_r (long numerador, long denominador) {
 
 int valido_r (struct racional r) {
 	if (r.den == 0)
-		return 0;
+	return 0;
 	return 1;
 }
 
--- prog1/tp1/tp1.c
+++ prog1/tp1/tp1.c
@@ -1,7 +1,7 @@
 /* 
- * Tipos Abstratos de Dados - TADs
- * Arquivo do programa principal, que usa o TAD racional.
- * Feito em 16/09/2024 para a disciplina CI1001 - Programação 1.
+* Tipos Abstratos de Dados - TADs
+* Arquivo do programa principal, que usa o TAD racional.
+* Feito em 16/09/2024 para a disciplina CI1001 - Programação 1.
 */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
@@ -11,40 +11,39 @@
 
 
 /* programa principal */
-int main ()
-{
-    int n, max, i;
-    struct racional r1, r2, sum, sub, mult, div;
-    srand (0); /* use assim, com zero */
-    scanf("%d %d", &n, &max);
-    for (i = 1; i <= n; i++) {
-        printf("%d: ", i);
-        r1 = sorteia_r(-max, max);
-        r2 = sorteia_r(-max, max);
-        imprime_r(r1);
-	printf(" ");
-        imprime_r(r2);
-	printf(" ");
-	if (valido_r(r1) == 0 || valido_r(r2) == 0) {
-            printf("NUMERO INVALIDO\n");
-            return 0;
-        }
-        sum = soma_r(r1, r2);
-        sub = subtrai_r(r1, r2);
-        mult = multiplica_r(r1, r2);
-        div = divide_r(r1, r2);
-        imprime_r(sum);
-	printf(" ");
-        imprime_r(sub);
-	printf(" ");
-        imprime_r(mult);
-	printf(" ");
-	if (r2.num == 0) {
-		printf("DIVISAO INVALIDA\n");
-		return 0;
+int main () {
+	int n, max, i;
+	struct racional r1, r2, sum, sub, mult, div;
+	srand(0); /* use assim, com zero */
+	scanf("%d %d", &n, &max);
+	for (i = 1; i <= n; i++) {
+		printf("%d: ", i);
+		r1 = sorteia_r(-max, max);
+		r2 = sorteia_r(-max, max);
+		imprime_r(r1);
+		printf(" ");
+		imprime_r(r2);
+		printf(" ");
+		if (valido_r(r1) == 0 || valido_r(r2) == 0) {
+			printf("NUMERO INVALIDO\n");
+			return 0;
+		}
+		sum = soma_r(r1, r2);
+		sub = subtrai_r(r1, r2);
+		mult = multiplica_r(r1, r2);
+		div = divide_r(r1, r2);
+		imprime_r(sum);
+		printf(" ");
+		imprime_r(sub);
+		printf(" ");
+		imprime_r(mult);
+		printf(" ");
+		if (r2.num == 0) {
+			printf("DIVISAO INVALIDA\n");
+			return 0;
+		}
+		imprime_r(div);
+		printf("\n");
 	}
-	imprime_r(div);
-        printf("\n");
-    }
-    return (0);
+	return (0);
 }
